9360742575b025c07daf2a2ab6538413c35d7506
Riyad Pathan <p47h4n@gmail.com>
2018-09-10 03:29:43 -0700

    Add files via upload

---
+++ SerialNumber VB.Net/SerialNumber/frmmain.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(183, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(229, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Get Serial Number"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(595, 301)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmmain"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+End Class
---
+++ SerialNumber VB.Net/SerialNumber/frmmain.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CompilerServices
+
+Public Class frmmain
+    Private Function SerialNumber() As String
+        Dim objectValue As Object = RuntimeHelpers.GetObjectValue(Interaction.GetObject("WinMgmts:", Nothing))
+        Dim text As String = ""
+        Dim objectValue2 As Object = RuntimeHelpers.GetObjectValue(NewLateBinding.LateGet(objectValue, Nothing, "InstancesOf", New Object() {"Win32_BaseBoard"}, Nothing, Nothing, Nothing))
+        Try
+            Dim enumerator As IEnumerator = CType(objectValue2, IEnumerable).GetEnumerator()
+            While enumerator.MoveNext()
+                Dim objectValue3 As Object = RuntimeHelpers.GetObjectValue(enumerator.Current)
+                text = Conversions.ToString(Operators.ConcatenateObject(text, Operators.ConcatenateObject(", ", NewLateBinding.LateGet(objectValue3, Nothing, "SerialNumber", New Object(-1) {}, Nothing, Nothing, Nothing))))
+            End While
+        Catch ex As Exception
+
+        End Try
+        Dim flag As Boolean = text.Length > 0
+        If flag Then
+            text = text.Substring(2)
+        End If
+        Return text
+    End Function
+
+    Private Sub frmmain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = SerialNumber()
+    End Sub
+End Class
